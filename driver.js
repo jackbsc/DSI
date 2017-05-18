78105e6f3798f758a4ef6f54fdf18e993dcf6e30
jackfan <quakeiii2012@gmail.com>
2017-05-19 01:26:23 +0800

    Change the low level drivers to use /dev/spidev* and /dev/i2c* instead of /dev/mem, increase security

--- driver.js
+++ driver.js
@@ -1,21 +1,19 @@
 //TODO: check that these can work with multiple device at the same time or multiple instance being initialized
 
-var rpio = require("rpio");
-var fs = require("fs");
-
 //Check if the board is supported
-var cpuinfo = fs.readFileSync("/proc/cpuinfo", "utf8");
+var cpuinfo = require("fs").readFileSync("/proc/cpuinfo", "utf8");
+
 if(cpuinfo.includes("Hardware") == false){
 	throw "Unable to Discover Board";
 }
 else{
-	var cpuPartNum = ["BCM2709"];
-	var modelName = ["Raspberry Pi 3"];
-	var cpu = cpuinfo.slice(cpuinfo.search("Hardware"));
-	cpu = cpu.substring(11, cpu.indexOf("\n"));
+	var cpuPartNum = ["BCM2709", "jetson_tx1"];
+	var modelName = ["Raspberry Pi 3", "Jetson TX1"];
+	cpuinfo = cpuinfo.slice(cpuinfo.search("Hardware"));
+	cpuinfo = cpuinfo.substring(11, cpuinfo.indexOf("\n"));
 	//Iterate through the list to find the model name
 	for(var i = 0; i < cpuPartNum.length; i++){
-		if(cpu === cpuPartNum[i]){
+		if(cpuinfo === cpuPartNum[i]){
 			process.stdout.write("Your Board is: ");
 			console.log(modelName[i]);
 			break;
@@ -24,54 +22,61 @@ else{
 	if(i >= cpuPartNum.length){
 		throw "Unsupported Board";
 	}
+
 }
 
-var driver = new Object(rpio.init({gpiomem: false}));
+var Gpio = require("onoff").Gpio;
+var spi = new Object(); // To be used as an 2D array of SPI objects, for multiple instance support
+var i2c = new Object(); // To be used as an array of I2C objects, for multiple instance support
+var driver = new Object();
+
+/* Generic Data Bus Configuration */
+
+/* SPI*/
+// Devices will share common bus configuration, unless different bus is used
+driver.initSPI = function(settings){
+	/* TODO: Test multiple SPI instances */
+	spi[settings.bus, settings.device] = require("spi-device").openSync(settings.bus, settings.device, {mode: 0, noChipSelect: true});
+
+}
+
+/* I2C */
+driver.initI2C = function(settings){
+	/* TODO: Test multiple I2C instances */
+	if(settings == undefined){
+		if(cpuinfo == "BCM2709"){
+			i2c[1] = require("i2c-bus").openSync(1);
+		}
+		else if(cpuinfo == "jetson_tx1"){
+			i2c[0] = require("i2c-bus").openSync(0);
+		}
+	}
+	else if(isNaN(settings.bus)){
+		throw "Please Specify I2C Bus Number, Leave Empty To Use Platform Specific";
+	}
+	else{
+		if(cpuinfo == "BCM2709" && (settings.bus <= 0 || settings.bus > 1)){
+			throw "The I2C Bus i2c-" + settings.bus + " does not exit";
+		}
+		// TODO: add in jetson check
+		i2c[settings.bus] = require("i2c-bus").openSync(settings.bus);
+	}
+}
 
 /*Microchip MCP3204 ADC driver*/
-driver.MCP3204 = function(pin){
+driver.MCP3204 = function(settings){
 
-	var cs = pin.cs;
-	var Vref = 3.3; //defualt to 3.3V Vref
-	var clk = 1000000; //default to 1MHz clk
-	var dataMode = 0; //default to mode 0
+	var cs = new Gpio(settings.cs, 'high');	
+	var Vref = 3.3; // Default to 3.3V Vref
 	var sampleMode;
 	var d0, d1;
 	var pos, neg;
-
-	//initialize the pin to output
-	rpio.open(cs, rpio.OUTPUT, rpio.HIGH);
+	
+	var bus = settings.bus, device = settings.device;
 
 	this.settings = function(settings){
 
 		//TODO: add in change Vref
-		rpio.spiBegin();
-
-		if(settings.clk == undefined){
-			//if no clk specify, use the old or default clock setting
-			settings.clk = clk;
-		}
-		else if(isNaN(settings.clk) || settings.clk < 0 || settings.clk > 250000000){
-			throw "MCP3204: Incorrect Clock Settings";
-		}
-		else{
-			//correct clock setting, remember it
-			clk = settings.clk;
-		}
-		rpio.spiSetClockDivider(250000000 / settings.clk);
-
-		if(settings.dataMode == undefined){
-			//if no data mode specify, use the old or defualt data mode setting
-			settings.dataMode = dataMode;
-		}
-		else if(isNaN(settings.dataMode) || settings.dataMode < 0 || settings.dataMode > 3){
-			throw "MCP3204: Incorrect Mode Settings";
-		}
-		else{
-			//correct data mode setting, remember it
-			dataMode = settings.dataMode;
-		}
-		rpio.spiSetDataMode(settings.dataMode);
 
 		sampleMode = settings.sampleMode;
 		if(sampleMode == "single"){
@@ -108,7 +113,7 @@ driver.MCP3204 = function(pin){
 			sampleMode = 0;	
 		}
 		else{
-			throw "MCP3204: Unrecognized Channel Configuration";
+			throw "MCP3204: Unrecognized Channel Confguration";
 		}	
 	}
 
@@ -117,21 +122,18 @@ driver.MCP3204 = function(pin){
 		var txbuf = new Buffer([cmd]);
 		var rxbuf = new Buffer(txbuf.length);
 
-		//pull the CS pin low
-		rpio.write(cs, rpio.LOW);	
-
+		// Pull the cs pin low
+		cs.writeSync(0);
 		//send start command
-		rpio.spiWrite(txbuf, txbuf.length);
+		spi[bus, device].transferSync([{sendBuffer: txbuf, receiveBuffer: rxbuf, byteLength: txbuf.length}]);
 		//send rest of the command
 		cmd = (d1 << 7) | (d0 << 6);
-		txbuf = new Buffer([cmd, 0xff]);
+		txbuf = new Buffer([cmd, 0xFF]);
 		rxbuf = new Buffer(txbuf.length);
-		rpio.spiTransfer(txbuf, rxbuf, txbuf.length);
-
-		//pull the CS pin high to end the transaction
-		rpio.write(cs, rpio.HIGH);
-
-		//return the read result
+		spi[bus, device].transferSync([{sendBuffer: txbuf, receiveBuffer: rxbuf, byteLength: txbuf.length}]);	
+		// Pull the cs pin back to high
+		cs.writeSync(1);
+		// Return the read result
 		return ((rxbuf[0] & 0x0F) << 8) | rxbuf[1];
 	}
 
@@ -224,34 +226,23 @@ driver.L293 = function(config){
 }
 
 /*Microchip TC74 temerature sensor driver*/
-driver.TC74 = function(){
+driver.TC74 = function(settings){
 	//TODO: check the clk speed limit of the i2c bus
 
 	var mode = "normal";
 	var tempReg = 0;
 	var configReg = 1;
+	var addr = settings.addr; // Need error checking here?
+	var bus = settings.bus; 
 
-	rpio.i2cBegin();
 	this.settings = function(settings){
-		if(isNaN(settings.addr) || settings.addr < 0 || settings.addr > 127){
-			throw "TC74: Error Slave Address";
-		}
-		rpio.i2cSetSlaveAddress(settings.addr);
-		if(isNaN(settings.clk) || settings.clk > 100000 || settings.clk < 0){
-			throw "TC74: Error I2C Clock Rate (~100kHz)";
-		}
-		rpio.i2cSetBaudRate(settings.clk);
+		// This function can use to do what?
+		// Generic settings of the temperature sensor?
 	}
 
 	this.getTemp = function(){
-		var rxbuf = new Buffer(1);
-		do{		
-			rpio.i2cWrite(Buffer([configReg]));
-			rpio.i2cRead(rxbuf, 1);
-		}while(!(rxbuf[0] & 0x40));
-		rpio.i2cWrite(Buffer([tempReg]));
-		rpio.i2cRead(rxbuf, 1);
-		return rxbuf[0];		
+		while(!(i2c[bus].readByteSync(addr, configReg) & 0x40));
+		return i2c[bus].readByteSync(addr, tempReg);		
 	}
 
 	this.standBy = function(){	
--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-var rpio = require("rpio");
+var sleep = require("sleep");
 var driver = require("./driver");
+driver.initSPI({bus: 0, device: 0}); // Since ADC uses SPI bus, initialize SPI
+driver.initI2C();
 
-var temp = new driver.TC74();
-var adc = new driver.MCP3204({cs:  11});
-var motor = new driver.L293({in1: 33, in2: 35, enable: 13});
+var temp = new driver.TC74({addr: 0x4C, bus: 1}); // Attach to i2c-1
+var adc = new driver.MCP3204({cs:  17, bus: 0, device: 0}); // Attach to spi0.0
+//var motor = new driver.L293({in1: 33, in2: 35, enable: 13});
 
 var humid = new Object();
 var lightInt = new Object();
@@ -18,13 +20,13 @@ lightInt.getInt = function(){
 	return -32.143 * adc.readVolts() + 106.07;
 }
 
-motor.setSpeed(1, 100, "anti_clockwise");
-temp.settings({addr: 0x4C, clk: 100000});
+//motor.setSpeed(1, 100, "anti_clockwise");
 
 while(1){
 
 	var hum = humid.getHumid().toFixed(2);
 	var int = lightInt.getInt().toFixed(2);
+	var tem = 0;
 	var tem = temp.getTemp();
 	var motorSpeed;
 	var motorStatus;
@@ -32,16 +34,16 @@ while(1){
 	if(hum < 70){
 		motorStatus = "COOLING";
 		if(tem > 25){
-			motor.setSpeed(1, 70, "clockwise");
+			//		motor.setSpeed(1, 70, "clockwise");
 			motorSpeed = "HIGH";
 		}
 		else{
-			motor.setSpeed(1, 20, "clockwise");
+			//		motor.setSpeed(1, 20, "clockwise");
 			motorSpeed = "LOW";
 		}
 	}
 	else{
-		motor.setSpeed(1, 50, "anti_clockwise");
+		//	motor.setSpeed(1, 50, "anti_clockwise");
 		motorStatus = "DRYING";
 	}
 
@@ -56,6 +58,6 @@ while(1){
 		process.stdout.write("     ");
 	}
 	process.stdout.write("\r");
-	rpio.msleep(500);
+	sleep.msleep(500);
 }
 
