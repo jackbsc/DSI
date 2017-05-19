952bfb3aac7f0aea384503b2371a3f328cc8d0cb
jackfan <quakeiii2012@gmail.com>
2017-05-19 23:09:39 +0800

    Added callback for ADC, but not tested

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ var driver = require("./driver");
 driver.initSPI({bus: 0, device: 0, clk: 1000000}); // Since ADC uses SPI bus, initialize SPI
 driver.initI2C({bus: 1});
 
-var temp = new driver.TC74({addr: 0x4C, bus: 1}); // Attach to i2c-1
-var adc = new driver.MCP3204({cs: 13, bus: 0, device: 0}); // Attach to spi0.0
+//var temp = new driver.TC74({addr: 0x4C, bus: 1}); // Attach to i2c-1
+var adc = new driver.MCP3204({cs: 13, bus: 0, device: 0, vref: 3.3}); // Attach to spi0.0
 //var motor = new driver.L293({in1: 33, in2: 35, enable: 13});
 
 var humid = new Object();
@@ -12,7 +12,10 @@ var lightInt = new Object();
 
 humid.getHumid = function(){
 	adc.settings({sampleMode: "single", ch: 1});
-	return (adc.readVolts() - 0.5) * 100 / (2.7 - 0.5);
+	//return (adc.readVolts() - 0.5) * 100 / (2.7 - 0.5);
+	return adc.readVolts(function(err, result){
+									console.log("callback!!!");
+									});
 }
 
 lightInt.getInt = function(){
@@ -23,11 +26,12 @@ lightInt.getInt = function(){
 //motor.setSpeed(1, 100, "anti_clockwise");
 
 while(1){
-
-	var hum = humid.getHumid().toFixed(2);
+	var hum = 0;
+	humid.getHumid();
+	//var hum = humid.getHumid().toFixed(2);
 	var int = lightInt.getInt().toFixed(2);
 	var tem = 0;
-	var tem = temp.getTemp();
+//	var tem = temp.getTemp();
 	var motorSpeed;
 	var motorStatus;
 
