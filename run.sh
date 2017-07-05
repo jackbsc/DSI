4350e0f6c9a240c6622f8d308cf1bd2e247c5b6d
jackbsc <quakeiii2012@gmail.com>
2017-07-06 05:18:06 +0800

    Changed to correct run capabilities

--- run.sh
+++ run.sh
@@ -3,6 +3,4 @@ docker run -it --rm \
            -v /sys/devices:/sys/devices \
            --device=/dev/spidev0.0 \
            --device=/dev/i2c-1 \
-           --cap-add=ALL \
-		   --privileged \
            jackbsc/iot:io
